9a3dd55879802c1487c04a34c87bb09a307fbfa6
arbol <arbol@RICARDOSOTELO>
2024-10-21 19:04:08 -0600

    Add project files.

---
+++ ActividadesWEB/ActividadesWEB/Models/Estudiantes.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActividadesWEB.Models
+{
+    public class Estudiante
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(50, ErrorMessage = "No puedes poner mas de 50 caracteres")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [MaxLength(50, ErrorMessage = "No puedes poner mas de 50 caracteres")]
+        [EmailAddress(ErrorMessage = "El correo debe ser valido")]
+        public string? Correo { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Calificacion no valida (0 - 10)")]
+        [Required(ErrorMessage = "La calificacion es obligatoria")]
+        public float? Calificacion { get; set; }
+    }
+}
---
+++ ActividadesWEB/ActividadesWEB/Models/Productos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ActividadesWEB.Models
+{
+    public class Productos
+    {
+        [Range(1, 9999, ErrorMessage = "Debe ser entero positivo mayor a 0")]
+        [Required(ErrorMessage = "Este campo es obligatorio y no puede estar vacio")]
+        public int Inventario { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio y no puede estar vacio")]
+        [MaxLength(200, ErrorMessage = "No puedes poner mas de 50 caracteres")]
+        public string? Nombre { get; set; }
+        public string? Departamento { get; set; }
+    }
+}
